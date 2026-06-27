d2c7c040dec78c2006ebd52a8d50f73aed78ecae
mhmdfirza <m.firza.aqila.r@gmail.com>
2026-06-27 19:48:44 +0700

    menambahkan beberapa mockdata

---
+++ storage/dummy/stores.jsx
@@ -0,0 +1,10 @@
+export const stores = [
+    { id: 1, name: 'Seapedia Official', avatar: 'https://i.pravatar.cc/150?u=store1', rating: 4.9, productCount: 1540, joinedAt: '2020-01-15', isOfficial: true },
+    { id: 2, name: 'Elektronik Murah', avatar: 'https://i.pravatar.cc/150?u=store2', rating: 4.6, productCount: 320, joinedAt: '2021-03-10', isOfficial: false },
+    { id: 3, name: 'Fashion Update', avatar: 'https://i.pravatar.cc/150?u=store3', rating: 4.8, productCount: 850, joinedAt: '2019-11-20', isOfficial: true },
+    { id: 4, name: 'Buku Kita', avatar: 'https://i.pravatar.cc/150?u=store4', rating: 4.7, productCount: 410, joinedAt: '2022-05-01', isOfficial: false },
+    { id: 5, name: 'Oto Parts', avatar: 'https://i.pravatar.cc/150?u=store5', rating: 4.5, productCount: 125, joinedAt: '2023-08-15', isOfficial: false },
+    { id: 6, name: 'Gaming Zone', avatar: 'https://i.pravatar.cc/150?u=store6', rating: 4.9, productCount: 200, joinedAt: '2021-07-22', isOfficial: true },
+    { id: 7, name: 'Home Living', avatar: 'https://i.pravatar.cc/150?u=store7', rating: 4.4, productCount: 630, joinedAt: '2020-09-05', isOfficial: false },
+    { id: 8, name: 'Sport Center', avatar: 'https://i.pravatar.cc/150?u=store8', rating: 4.6, productCount: 380, joinedAt: '2022-12-10', isOfficial: true },
+];
